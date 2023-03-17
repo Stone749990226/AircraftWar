50994b2a16b62668515d5cc236182c942dc53162
stone <749990226@qq.com>
2023-03-17 20:59:00 +0800

    实验二

--- src/edu/hitsz/aircraft/BombPropertyFactory.java
+++ src/edu/hitsz/aircraft/BombPropertyFactory.java
@@ -3,6 +3,6 @@ package edu.hitsz.aircraft;
 public class BombPropertyFactory implements PropertyFactory {
     @Override
     public AbstractProperty createProperty(int locationX, int locationY) {
-        return new BloodProperty(locationX, locationY);
+        return new BombProperty(locationX, locationY);
     }
 }
--- src/edu/hitsz/aircraft/BulletPropertyFactory.java
+++ src/edu/hitsz/aircraft/BulletPropertyFactory.java
@@ -3,6 +3,6 @@ package edu.hitsz.aircraft;
 public class BulletPropertyFactory implements PropertyFactory{
     @Override
     public AbstractProperty createProperty(int locationX, int locationY) {
-        return new BloodProperty(locationX, locationY);
+        return new BulletProperty(locationX, locationY);
     }
 }
